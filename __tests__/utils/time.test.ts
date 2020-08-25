9fb88bc961aa1da456ce19fadfa0335445b2f596
eomttt <hyunt0413@gmail.com>
2020-08-25 11:14:50 +0900

    Test case 추가

--- __tests__/utils/time.test.ts
+++ __tests__/utils/time.test.ts
@@ -1,8 +1,13 @@
 import { getTimeFormat } from 'utils/time';
 
 it('Time test', () => {
-  describe('Get time format', () => {
+  describe('Get time format under 10', () => {
     const result = getTimeFormat(3);
     expect(result).toEqual('03');
+  });
+
+  describe('Get time format over 10', () => {
+    const result = getTimeFormat(30);
+    expect(result).toEqual(30);
   });
 });
