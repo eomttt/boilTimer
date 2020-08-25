cbe81ff54072c97dda76e341dcb6bf80362025fe
eomttt <hyunt0413@gmail.com>
2020-08-25 09:38:58 +0900

    Run Test

--- src/pages/Timer/index.tsx
+++ src/pages/Timer/index.tsx
@@ -2,23 +2,32 @@ import { Button } from 'components/Button';
 import { Layout } from 'components/Layout';
 import { Timer as TimerComponent } from 'components/Timer';
 import { TimerStatus } from 'constants/time';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { View } from 'react-native';
 import Styles from './styles';
-
-export const STORE_TIME = 0.1;
+import BoiledDatabase from 'helpers/BoiledDatabase';
+import { BoiledTime, BoiledIndicate } from 'constants/boiled';
 
 export const Timer = () => {
+  const [storedTime, setStoredTime] = useState(0);
   const [timerStatus, setTimerStatus] = useState(TimerStatus.STOP);
 
   const startTimer = useCallback(() => {
     setTimerStatus(TimerStatus.START);
   }, []);
 
+  useEffect(() => {
+    const savedIndicator = BoiledDatabase.getIndicator();
+    if (savedIndicator.length > 0) {
+      // TODO: get BoiledIndicate from savedIndicator
+      setStoredTime(BoiledTime[BoiledIndicate.SOFT]);
+    }
+  }, []);
+
   return (
     <Layout>
       <View style={Styles.timer}>
-        <TimerComponent remainTime={STORE_TIME} status={timerStatus} />
+        <TimerComponent remainTime={storedTime} status={timerStatus} />
       </View>
       <View style={Styles.button}>
         <Button label={'시작'} onClick={startTimer} />
